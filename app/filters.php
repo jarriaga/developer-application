7623b9cd93bebeff4a015d34fac6cc9aa3738cff
Jesus <jarriagabarron@gmail.com>
2014-12-20 18:06:56 -0600

    resolve filter problem

--- app/filters.php
+++ app/filters.php
@@ -13,7 +13,7 @@
 
 App::before(function($request)
 {
-	App::abort(403, 'You aren\'t doing it right.');
+	//App::abort(403, 'You aren\'t doing it right.');
 });
 
 
@@ -95,5 +95,5 @@ Route::filter('check-query', function()
 	if (!is_null(Input::query('name'))) {
 		Session::flash('error', 'Why would you submit a GET form...');
 	}
-	User::all(); // this is the problem - erase it. `User` is pointless in the app.
+	//User::all(); // this is the problem - erase it. `User` is pointless in the app.
 });
